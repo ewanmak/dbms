9dd6932314ca882d87f6c12e8fd503b6b3b4c77a
ewanmak <ewanmym@gmail.com>
2026-01-08 23:53:43 +0800

    Create hammerdb_tpc_c.tcl

---
+++ benchmarks/hammerdb_tpc_c.tcl
@@ -0,0 +1,7 @@
+dbset db mysql
+dbset bm TPC-C
+diset connection mysql_host localhost
+diset connection mysql_user root
+diset connection mysql_pass password
+diset tpcc mysql_count_ware 10
+loadscript
